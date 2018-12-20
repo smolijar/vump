2deee1f8a448672cf5cc1f200be5554ba977c136
Jaroslav Šmolík <grissius@gmail.com>
2018-12-20 22:34:35 +0100

    Refactor vump tests

--- spec/vump/vump_spec.rb
+++ spec/vump/vump_spec.rb
@@ -31,37 +31,36 @@ class IrrelevantTestModule
     false
   end
 end
-class Vump::Vump
-  def all_modules
-    [
-      TestModule,
-      IrrelevantTestModule
-    ]
-  end
-end
 vump = Vump::Vump.new('/', :minor, {})
 vump.logger.logger.level = Logger::UNKNOWN
 
 RSpec.describe vump.class.name, type: :request do
-  before :each do
-    mods = [
-      TestModule,
-      IrrelevantTestModule
-    ]
-    allow_any_instance_of(Vump::Vump).to receive(:all_modules).and_return(mods)
+  context 'real modules' do
+    it 'all_modules' do
+      expect(vump.all_modules).to be_an_instance_of(Array)
+    end
   end
-  it 'load_modules' do
-    expect(vump.load_modules.length).to be(1)
-  end
-  it 'read_versions' do
-    expect(vump.read_versions(vump.load_modules)).to match_snapshot
-  end
-  it 'select_version' do
-    expect(vump.select_version(%w[a b c])).to be_falsy
-    expect(vump.select_version(%w[a])).to be_truthy
-  end
-  it 'start' do
-    vump.start
-    expect(TestModule.last_write).to eq('0.1.0')
+  context 'fake modules' do
+    before :each do
+      mods = [
+        TestModule,
+        IrrelevantTestModule
+      ]
+      allow_any_instance_of(Vump::Vump).to receive(:all_modules).and_return(mods)
+    end
+    it 'load_modules' do
+      expect(vump.load_modules.length).to be(1)
+    end
+    it 'read_versions' do
+      expect(vump.read_versions(vump.load_modules)).to match_snapshot
+    end
+    it 'select_version' do
+      expect(vump.select_version(%w[a b c])).to be_falsy
+      expect(vump.select_version(%w[a])).to be_truthy
+    end
+    it 'start' do
+      vump.start
+      expect(TestModule.last_write).to eq('0.1.0')
+    end
   end
 end
