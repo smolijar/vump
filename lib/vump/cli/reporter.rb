91f0ac314de3200a6697439304124ca229d080dc
Jaroslav Šmolík <grissius@gmail.com>
2019-02-13 19:17:37 +0100

    Replace guards with early returns

--- lib/vump/cli/reporter.rb
+++ lib/vump/cli/reporter.rb
@@ -24,41 +24,41 @@ module Vump
     end
 
     def report_preamble(base_path, arg, options)
-      if @level <= ::Logger::INFO
-        header title: 'Vump', width: 80, align: 'center', rule: true, color: 'yellow', bold: true, timestamp: true
-        table(border: false) do
-          row do
-            column('Base path', width: 10)
-            column(base_path, width: 30)
-          end
-          row do
-            column('Argument', width: 10)
-            column(arg, width: 30)
-          end
-          row do
-            column('Options', width: 10)
-            column(options, width: 30)
-          end
+      return if @level > ::Logger::INFO
+
+      header title: 'Vump', width: 80, align: 'center', rule: true, color: 'yellow', bold: true, timestamp: true
+      table(border: false) do
+        row do
+          column('Base path', width: 10)
+          column(base_path, width: 30)
+        end
+        row do
+          column('Argument', width: 10)
+          column(arg, width: 30)
+        end
+        row do
+          column('Options', width: 10)
+          column(options, width: 30)
         end
       end
     end
 
     def report_module_overview
-      if @level <= ::Logger::INFO
-        header title: 'Version module overview', align: 'center', width: 80
-        table(border: true) do
+      return if @level > ::Logger::INFO
+
+      header title: 'Version module overview', align: 'center', width: 80
+      table(border: true) do
+        row do
+          column('NAME', width: 20)
+          column('STATUS', width: 30)
+          column('READ VERSION', width: 20)
+        end
+        @modules.each do |name, data|
           row do
-            column('NAME', width: 20)
-            column('STATUS', width: 30)
-            column('READ VERSION', width: 20)
-          end
-          @modules.each do |name, data|
-            row do
-              rel = data[:relevant]
-              column(name)
-              column(rel ? 'relevant' : 'loaded', color: rel ? 'green' : nil)
-              column(data[:read_version], color: rel ? 'green' : nil)
-            end
+            rel = data[:relevant]
+            column(name)
+            column(rel ? 'relevant' : 'loaded', color: rel ? 'green' : nil)
+            column(data[:read_version], color: rel ? 'green' : nil)
           end
         end
       end
