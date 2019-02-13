1146a5c562736446783e4c3669f11f85a729cd93
Jaroslav Šmolík <grissius@gmail.com>
2019-02-13 19:19:20 +0100

    Fix version module test

--- spec/vump/vump_spec.rb
+++ spec/vump/vump_spec.rb
@@ -25,7 +25,7 @@ class TestModule
   end
 end
 
-class IrrelevantTestModule
+class IrrelevantTestModule < Vump::BaseVersionModule
   def initialize(base_dir); end
 
   def relevant?
@@ -46,7 +46,7 @@ RSpec.describe vump.class.name do
     before :each do
       mods = [
         TestModule,
-        IrrelevantTestModule
+        IrrelevantTestModule,
       ]
       allow_any_instance_of(Vump::Vump).to(
         receive(:all_modules).and_return(mods)
