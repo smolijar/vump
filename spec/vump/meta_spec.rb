cbb4525563457305bd9aa17de28246713b428907
Jaroslav Šmolík <grissius@gmail.com>
2019-02-14 07:12:42 +0100

    Fix semver tests

--- spec/vump/meta_spec.rb
+++ spec/vump/meta_spec.rb
@@ -2,8 +2,11 @@ require 'vump/meta'
 
 RSpec.describe 'Vump meta data' do
   it 'works and is unchanged' do
-    meta = %i[version author name description email license homepage summary]
+    meta = %i[author name description email license homepage summary]
            .map { |m| [m, Vump::Meta.public_send(m)] }.to_h
     expect(meta).to match_snapshot
+  end
+  it 'version returns semver' do
+    expect(Vump::Meta.version).to match(/[0-9]+\.[0-9]+\.[0-9]+/)
   end
 end
